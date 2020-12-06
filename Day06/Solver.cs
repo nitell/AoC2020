6f12468d80745707b4ce4a4d29c4f1d79093f321
Mikael Nitell <mikael_nitell@hotmail.com>
2020-12-06 22:11:15 +0100

    Day6

---
+++ Day06/Solver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AoC.Day06
+{
+    class Solver
+    {
+        internal string Solve1()
+        {
+            var records = File.ReadAllText(@"Day06\input.txt").Split(Environment.NewLine + Environment.NewLine);
+            return records.Sum(r => r.Replace(Environment.NewLine, String.Empty).Distinct().Count()).ToString();            
+        }
+
+        internal string Solve2()
+        {
+            var records = File.ReadAllText(@"Day06\input.txt").Split(Environment.NewLine + Environment.NewLine);
+            return records.Sum(r => r.Split(Environment.NewLine).Aggregate((a, b) => new String(a.Intersect(b).ToArray())).Count()).ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AoC
     {
         static void Main(string[] args)
         {
-            var o = new Day05.Solver();
+            var o = new Day06.Solver();
             Console.WriteLine(o.Solve1());
             Console.WriteLine(o.Solve2());
 
